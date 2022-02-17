27688c8e4e467dc3e55efdd4148e32075993c515
victor <victor.tm90@gmail.com>
2022-02-17 18:43:41 +0100

    it5, 4 no sale y no sabemos pk. it 6, planteada

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-  return  Recipe.create({
-     title: "Tomate Relleno",
-     level:"Easy Peasy",
-     ingridients: "tomatos with love",
-     cuisine: "heladera",
-     dishType: "main_course",
-     duration: 3,
-     creator: "Mi Abuela Ana",
-   })
+    return Recipe.create({
+      title: "Tomate Relleno",
+      level: "Easy Peasy",
+      ingridients: "tomatos with love",
+      cuisine: "heladera",
+      dishType: "main_course",
+      duration: 3,
+      creator: "Mi Abuela Ana",
+    });
+  })
+  .then((response) => {
+    console.log(response.title);
+
+    return Recipe.insertMany(data);
+  })
+  .then((response) => {
+    console.log(response)
+    //  response.forEach((elem) => {
+    //   console.log(elem.title);
+    // });   
     
-  })
-  .then ((response) => {
-    console.log(response.title)
-
-    return Recipe.insertMany(data)
-  })
-  .then ((response) => {
-     return response.forEach((elem)=> {
-       console.log(elem.title)
-     })
+   // Recipe.findOneAndUpdate ( { title: "Rigatoni alla Genovese" }, { duration: 100 }, {new: true} ); //! SALE NULL Y NO SABEMOS PORQUÉ !!!!!!!!!!!!!!
    
+   return Recipe.deleteOne({title: "Carrot Cake"})
+
+  })
+  .then((response) => { 
+   console.log("me cargué la zanahoria ", response);
+    return  mongoose.connection.close(MONGODB_URI)
+  })
+  .then((response)=>{
+    console.log("connexion close")
   })
 
-
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
